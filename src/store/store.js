f541a863f90dd859cf2a8a6817646d707461f4ca
MohamedHamed12 <mh1779371@gmail.com>
2025-02-03 12:14:43 +0000

    add cart

--- src/store/store.js
+++ src/store/store.js
@@ -5,12 +5,14 @@ import categoryReducer from './slices/categorySlice';
 import productReducer from './slices/productSlice';
 import authReducer from './slices/authSlice';
 import bannerReducer from './slices/bannerSlice';
+import cartReducer from './slices/CartSlice';
 const store = configureStore({
   reducer: {
     category: categoryReducer,
     products: productReducer, // This must match the slice name used in useSelector
     auth: authReducer,
-    banner: bannerReducer
+    banner: bannerReducer,
+     cart: cartReducer
   },
 });
 
