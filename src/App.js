2ce168ca79fc906607dc007118737f9fc6168bb3
MoHamed8200 <mohamoudh477@gmail.com>
2025-02-04 15:11:26 +0000

    add cart page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 // import ProductDetails from './pages/ProductDetails';
-import ProductPage from './pages/ProjectPage';
+import ProductPage from './pages/ProductPage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Cart from './pages/CartPage';
 function App() {
   
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route path="/product/:productId" element={<ProductPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
