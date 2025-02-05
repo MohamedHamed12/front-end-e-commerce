a50ec2ec5754dad49e57ae9d94d4e86175951b55
Mohamed Hamed <90472426+MohamedHamed12@users.noreply.github.com>
2025-02-05 09:35:15 +0200

    Revert "add cart"
    This reverts commit 28c1c745ae27fd8dea193092433746fd44e06626.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
+
+
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store/slices/authSlice';
-import CartIcon from './CartIcon'; // Import the new CartIcon component
-
 const Navbar = () => {
   const { userData, isAuthenticated } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -14,30 +14,31 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="container mx-auto fix">
+      <div className="container mx-auto">
+
         <div className="flex justify-between items-center py-4">
           <h1 className="text-2xl font-bold">E-commerce</h1>
 
-          <div className="flex space-x-4">
+
+          <div className=" flex space-x-4">
             <Link to="/" className="hover:underline">Home</Link>
             <Link to="/" className="hover:underline">About</Link>
             <Link to="/" className="hover:underline">Contact</Link>
           </div>
-
- 
-          <CartIcon />
-
-          {isAuthenticated ? (
-            <>
-              <span>Welcome, {userData.user.name}</span>
-              <button onClick={handleLogout} className="bg-red-500 p-2 rounded">Logout</button>
-            </>
-          ) : (
-            <div className="flex space-x-4 items-end">
-              <Link to="/login" className="hover:underline">Login</Link>
-              <Link to="/signup" className="hover:underline">Sign Up</Link>
-            </div>
-          )}
+             {isAuthenticated ? (
+              <>
+                <span>Welcome, {userData.user.name}</span>
+                <button onClick={handleLogout} className="bg-red-500 p-2 rounded">Logout</button>
+              </>
+            ) : (
+              <>
+                <div className="flex space-x-4 items-end">
+                  <Link to="/login" className="hover:underline">Login</Link>
+                  <Link to="/signup" className="hover:underline">Sign Up</Link>
+                </div>
+              </>
+            )
+            }
         </div>
       </div>
       <div className="border-b border-gray-300 w-full"></div>
